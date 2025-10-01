6e84a4c916bbcfdfef7b0083e0452c9040594f9e
ArtemGuskov <banzai1302@mail.ru>
2025-10-01 11:53:05 +0300

    Add files via upload

--- project1231.cpp
+++ project1231.cpp
@@ -8,7 +8,7 @@ int main()
     setlocale(LC_ALL, "Russian");
     float x, y;
     cin >> x >> y;
-    float R = sqrt((abs(sin(x) / y), 4));
+    float R = sqrt(sqrt(abs(sin(x) / y)));
     float S = log10(pow(2, y));
     cout << "R = " << R << ", S = " << S << endl;
     float C = max(R, S);
